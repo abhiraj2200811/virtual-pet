da9b38b586aeed2ceae0b127f99f140e46c3116b
abhiraj2200811 <70210909+abhiraj2200811@users.noreply.github.com>
2020-11-12 22:41:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,70 @@
+//Create variables here
+var dog_sit,dog, happyDog, database, foodS, foodStock;
+
+function preload()
+{
+  //load images here
+  dog_sit = loadImage("images/dogImg.png");
+  happyDog = loadImage("images/dogImg1.png")
+
+}
+
+function setup() {
+  createCanvas(500, 500);
+  
+  dog = createSprite(250,250,50,50)
+  dog.addImage(dog_sit);
+
+  database=firebase.database();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock)
+
+}
+
+
+function draw() {  
+
+background(46,139,87)
+
+if(keyWentDown(UP_ARROW)){
+
+writeStock(foodS);
+dog.addImage(dogHappy);
+
+}
+
+  drawSprites();
+  //add styles here
+
+  textSize="5"
+  fill("white")
+  stroke(5)
+
+  text("Press Up Arrow key to feed Woody Milk.", 240, 200 )
+
+}
+
+function readSock(data){
+
+foodS=data.val();
+
+}
+
+function writeStock(x){
+
+if(x<=0){
+
+  x=0;
+
+}else{
+
+x=x-1;
+
+}
+
+database.ref('/').update({
+  Food:x
+})
+
+}
